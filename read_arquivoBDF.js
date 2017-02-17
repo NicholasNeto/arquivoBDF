ecb96d2bf9820cf45c92e6ce53c95af82ff8e7f7
Nicholas <nicholasneto@outlook.com>
2017-02-17 10:26:23 -0200

    [Nicholas] Switch to variable callback

--- read_arquivoBDF.js
+++ read_arquivoBDF.js
@@ -1,12 +1,20 @@
 var Noticia = require('./projectArquivo');
 
+// Neste Script os callbacks não são anonimos, eles estão em variavel ex. 
 var readinDataBase = function(callback) {
 
-	Noticia.find({}, function(err, resultNoticia) {
+	var callbackFind = function(err, resultNoticia) {
   		if (err) throw err;
+  		
+  		callback(null, resultNoticia)
+ 	}
 
-  		callback(null, resultNoticia)		
-  	});
+	Noticia.find({}, callbackFind);
 };
+
+// Esta chamada é para rodar o script sozinho  
+// readinDataBase(function(erro , resul ) {
+//	console.log(resul)
+// });
 
 module.exports = readinDataBase;
